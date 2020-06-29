3e8f3e5a0f9f14ddfc4db96a8d4908fe2ee953c3
matteoLorenzini <matteo-lorenzini@outlook.it>
2020-06-29 14:17:43 +0200

    Updated path according to local settings

--- JavaFrameworkProgram/XsltTransformer.java
+++ JavaFrameworkProgram/XsltTransformer.java
@@ -12,12 +12,12 @@ import java.net.URISyntaxException;
 
 public class XsltTransformer {
     public static void main(String[] args) throws IOException, URISyntaxException, TransformerException {
-            String xmlFile = "/Users/mac-pro/GTA_Repo/ProfileParser/Profiles/Place/Livorno.xml"; //put path of input XML file between ""
-            String xslFile = "/Users/mac-pro/GTA_Repo/ProfileParser/Profiles/Place/Place.xsl"; //put path of input XSL file between ""
+            String xmlFile = "/home/matteolorenzini/GTARepo/ProfileParser/Profiles/Place/fake-canton-annotated-example_v2_20200629.xml"; //put path of input XML file between ""
+            String xslFile = "/home/matteolorenzini/GTARepo/ProfileParser/Profiles/Place/Place.xsl"; //put path of input XSL file between ""
             TransformerFactory factory = TransformerFactory.newInstance();
             Source xslt = new StreamSource(new File(xslFile));
             Transformer transformer = factory.newTransformer(xslt);
             Source text = new StreamSource(new File(xmlFile));
-            transformer.transform(text, new StreamResult(new File("/Users/mac-pro/GTA_Repo/ProfileParser/Profiles/Place/target_new.xml"))); //put path of newly created XML file between ""
+            transformer.transform(text, new StreamResult(new File("/home/matteolorenzini/GTARepo/ProfileParser/Profiles/Place/target_new_v2.xml"))); //put path of newly created XML file between ""
     }
 }
