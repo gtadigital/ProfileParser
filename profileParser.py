8995724430b88818f97f0ddce06c653641e6d1ac
matteoLorenzini <matteo-lorenzini@outlook.it>
2021-05-17 18:09:52 +0200

    Updated folder definition

--- profileParser.py
+++ profileParser.py
@@ -21,10 +21,10 @@ if __name__ == "__main__":
 
     #we want to store the output files in subdirectories of maximal 8MB
     #we create the first subdirectory
-    current_dir = myOutput+ "\\Folder"+ str(0) + "\\"    #current_dir stores path of current subdirectory
+    current_dir = myOutput+ "//Folder"+ str(0) + "//"    #current_dir stores path of current subdirectory
     index_dir=1
     while os.path.exists(current_dir): #check if subdirectory already exists, if yes we presume it is already full and create new one
-        current_dir = myOutput+ "\\Folder"+ str(index_dir) + "\\"
+        current_dir = myOutput+ "//Folder"+ str(index_dir) + "//"
         index_dir+=1
     os.makedirs(current_dir)
     file_list= []   #in this list we will store the elements of the current subdirectory. This helps us to keep track of its current size
@@ -47,10 +47,10 @@ if __name__ == "__main__":
                 #if yes: create new subdirectory, update current_dir variable (s.t. the new file will be added to the new subdirectory), reset file_list
                 file_list.append(infile)
                 if sum(len(f) for f in file_list) > 8000000:
-                    current_dir = myOutput+ "\\Folder"+ str(index_dir) + "\\"
+                    current_dir = myOutput+ "//Folder"+ str(index_dir) + "//"
                     index_dir+=1
                     while os.path.exists(current_dir):
-                        current_dir = myOutput+ "\\Folder"+ str(index_dir) + "\\"
+                        current_dir = myOutput+ "//Folder"+ str(index_dir) + "//"
                         index_dir+=1
                     os.makedirs(current_dir)
                     file_list=[]
